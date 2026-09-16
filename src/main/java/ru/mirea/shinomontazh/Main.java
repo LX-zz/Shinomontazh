c2d2c7c73dacd241f9131e519183fb3c9b2c5cd5
Кирпичев Кирилл Евгеньевич <gollfifa1@gmail.com>
2026-09-16 19:26:18 +0300

    Добавил полчение заказов из базы данных

--- src/main/java/ru/mirea/shinomontazh/Main.java
+++ src/main/java/ru/mirea/shinomontazh/Main.java
@@ -1,37 +1,68 @@
+// package ru.mirea.shinomontazh;
+
+// import ru.mirea.shinomontazh.model.Client;
+// import ru.mirea.shinomontazh.service.ClientService;
+// import java.util.List;
+
+
+// public class Main {
+//     public static void main(String[]args) {
+//         System.out.println("Система шиномонтажа запущена");
+
+//         ClientService clientService = new ClientService();
+
+//         try {
+
+//             List<Client> clients = clientService.getAllClients();
+
+//             System.out.println("Список клиентов:");
+
+//             for (Client client : clients) {
+
+//                 System.out.println(
+//                         client.getId() + " | " +
+//                         client.getFullName() + " | " +
+//                         client.getPhone() + " | " +
+//                         client.getEmail()
+//                 );
+//             }
+
+//         } catch (Exception e) {
+
+//             System.out.println("Ошибка:");
+//             System.out.println(e.getMessage());
+
+//         }
+//     }
+// }
 package ru.mirea.shinomontazh;
 
-import ru.mirea.shinomontazh.model.Client;
-import ru.mirea.shinomontazh.service.ClientService;
+import ru.mirea.shinomontazh.model.WorkOrder;
+import ru.mirea.shinomontazh.repository.WorkOrderRepository;
+
 import java.util.List;
 
-
 public class Main {
-    public static void main(String[]args) {
-        System.out.println("Система шиномонтажа запущена");
 
-        ClientService clientService = new ClientService();
+    public static void main(String[] args) {
+
+        WorkOrderRepository repository = new WorkOrderRepository();
 
         try {
 
-            List<Client> clients = clientService.getAllClients();
+            List<WorkOrder> orders = repository.getAll();
 
-            System.out.println("Список клиентов:");
+            System.out.println("Список заказов:");
+            System.out.println();
 
-            for (Client client : clients) {
-
-                System.out.println(
-                        client.getId() + " | " +
-                        client.getFullName() + " | " +
-                        client.getPhone() + " | " +
-                        client.getEmail()
-                );
+            for (WorkOrder order : orders) {
+                System.out.println(order);
             }
 
         } catch (Exception e) {
 
             System.out.println("Ошибка:");
             System.out.println(e.getMessage());
-
         }
     }
 }
---
+++ src/main/java/ru/mirea/shinomontazh/repository/WorkOrderRepository.java
@@ -0,0 +1,64 @@
+package ru.mirea.shinomontazh.repository;
+
+import ru.mirea.shinomontazh.model.OrderStatus;
+import ru.mirea.shinomontazh.model.WorkOrder;
+import ru.mirea.shinomontazh.util.DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkOrderRepository {
+
+    public List<WorkOrder> getAll() throws Exception {
+
+        List<WorkOrder> orders = new ArrayList<>();
+
+        String sql = "SELECT * FROM orders ORDER BY id";
+
+        try (
+                Connection connection = DatabaseManager.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet resultSet = statement.executeQuery()
+        ) {
+
+            while (resultSet.next()) {
+
+                WorkOrder order = new WorkOrder();
+
+                order.setId(resultSet.getInt("id"));
+                order.setCarBrand(resultSet.getString("car_brand"));
+                order.setCarNumber(resultSet.getString("car_number"));
+
+                order.setServiceName(
+                        resultSet.getString("service_type")
+                );
+
+                order.setStatus(
+                        OrderStatus.valueOf(
+                                resultSet.getString("status")
+                        )
+                );
+
+                order.setPrice(
+                        resultSet.getBigDecimal("price")
+                );
+
+                order.setCreateDate(
+                        resultSet.getTimestamp("create_date")
+                                .toLocalDateTime()
+                );
+
+                order.setClientId(
+                        resultSet.getInt("client_id")
+                );
+
+                orders.add(order);
+            }
+        }
+
+        return orders;
+    }
+}
